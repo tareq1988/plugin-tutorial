34abe6f8f2d9cdd04c1b006239197abb1d0f5356
Tareq Hasan <tareq1988@gmail.com>
2014-07-01 01:30:52 +0600

    initial commit

---
+++ wetuts.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: We Tuts
+ * Description: A simple description of our plguin
+ * Plugin URI: http://tareq.weDevs.com
+ * Author: Tareq Hasan
+ * Author URI: http://tareq.wedevs.com
+ * Version: 0.1
+ * License: GPL2
+ * Text Domain: wetuts
+ */
+
+/**
+ * Copyright (c) 2014 Tareq Hasan (email: tareq@wedevs.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
+
+// don't call the file directly
+if ( !defined( 'ABSPATH' ) ) exit;
